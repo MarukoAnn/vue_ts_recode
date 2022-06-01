ecebd8754586fdc600f372230562b679ea757b09
黄慧芳 <1974591593@qq.com>
2022-06-01 15:41:28 +0800

    feat: 引入windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import WindiCSS from 'vite-plugin-windicss'
+
 // https://vitejs.dev/config/
-export default defineConfig ({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.join(path.resolve(__dirname), './src')
-		}
-	},
-	base: './',
-	server: {
-		port: 4000,
-		open: true,
-		cors: true
-	}
+export default defineConfig({
+  plugins: [vue(), WindiCSS()],
+  resolve: {
+    alias: {
+      '@': path.join(path.resolve(__dirname), './src')
+    }
+  },
+  base: './',
+  server: {
+    port: 4000,
+    open: true,
+    cors: true
+  }
 })
