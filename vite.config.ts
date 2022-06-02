fe4fe35f638f720fb9fa8aac1099d4a1387ae820
黄慧芳 <1974591593@qq.com>
2022-06-02 10:36:47 +0800

    fix: 服务器代理

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,13 @@ export default defineConfig({
   server: {
     port: 4000,
     open: true,
-    cors: true
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://api.moonshinean.com/', // 代理地址，这里设置的地址会代替axios中设置的baseURL
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        rewrite: (a: string) => a.replace(/^\/api/, '')
+      }
+    }
   }
 })
